1653dd06df7a8634d6b53b53f733fe11e6842fcb
Naimur Reza <naimur.rezaa@gmail.com>
2023-07-04 20:57:02 +0600

    done with show messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
 const socket = io.connect("http://localhost:5000");
 const App = () => {
+  const [message, setMessage] = useState("");
+  const [data, setData] = useState("");
   const sendMessage = () => {
-    socket.emit("send_message", { message: "He wrote Notir Pola" });
+    socket.emit("send_message", { message: message });
   };
   useEffect(() => {
     socket.on("receive_message", (data) => {
-      console.log(data);
+      setData(data.message);
     });
   }, [socket]);
   return (
     <div>
-      <input type="text" placeholder="message..." />
+      <input
+        type="text"
+        onChange={(e) => setMessage(e.target.value)}
+        placeholder="message..."
+      />
       <button onClick={sendMessage}>Send Message</button>
+      <h1>{data}</h1>
     </div>
   );
 };
