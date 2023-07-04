d432ec84d92985f6d5a193fcf235005f52950516
Naimur Reza <naimur.rezaa@gmail.com>
2023-07-04 21:53:13 +0600

    done room integrations

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,38 @@ const socket = io.connect("http://localhost:5000");
 const App = () => {
   const [message, setMessage] = useState("");
   const [data, setData] = useState("");
+  const [room, setRoom] = useState("");
   const sendMessage = () => {
-    socket.emit("send_message", { message: message });
+    socket.emit("send_message", { message: message, room: room });
   };
+
+  // sockets for joining room
+  const joinRoom = () => {
+    if (room !== "") {
+      socket.emit("join_room", room);
+    }
+  };
+
   useEffect(() => {
     socket.on("receive_message", (data) => {
-      setData(data.message);
+      setData(data);
     });
   }, [socket]);
   return (
     <div>
       <input
         type="text"
+        onChange={(e) => setRoom(e.target.value)}
+        placeholder="enter room id"
+      />
+      <button onClick={joinRoom}>Join Room</button>
+      <input
+        type="text"
         onChange={(e) => setMessage(e.target.value)}
         placeholder="message..."
       />
       <button onClick={sendMessage}>Send Message</button>
-      <h1>{data}</h1>
+      <ul>{data.message}</ul>
     </div>
   );
 };
