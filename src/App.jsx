6f67096011d31083c7afafd49c9b798fe5f2384d
Naimur Reza <naimur.rezaa@gmail.com>
2023-07-06 14:41:06 +0600

    added new colors

--- src/App.jsx
+++ src/App.jsx
@@ -53,13 +53,17 @@ const App = () => {
       setMessageList((previousMessages) => [...previousMessages, data]);
       console.log(data);
     });
-    // return () => {
-    //   socket.disconnect();
-    // };
 
     socket.on("user_connected", (data) => {
       setConnectedUsers(data.length);
     });
+
+    return () => {
+      socket.disconnect();
+      socket.on("user_connected", (data) => {
+        setConnectedUsers(data.length);
+      });
+    };
   }, [socket]);
 
   const handleForm = (e) => {
@@ -81,39 +85,39 @@ const App = () => {
   };
   console.log(connectedUsers);
   return (
-    <div className="h-[100vh]  overflow-y-scroll flex items-center justify-center bg-gradient-to-r from-black via-red-950 to-black px-5">
+    <div className="lg:h-[100vh] min-h-[90vh]   flex items-center justify-center  px-5">
       <form
         onSubmit={handleForm}
-        className=" p-5 space-y-2 relative rounded-lg lg:w-1/2 w-full shadow-xl bg-white/5 backdrop-blur-md">
-        {!showMessage && (
-          <h1 className="text-xl font-semibold text-gray-200">
-            Please fill these field to start the chat
-          </h1>
-        )}
+        className=" p-5 space-y-2 bg-gradient-to-r from-pink-600 via-red-800 to-violet-700 relative rounded-lg lg:w-1/2 w-full shadow-xl bg-white/5 backdrop-blur-md">
         {!showMessage && (
           <>
-            <input
-              required
-              type="text"
-              className="bg-black/60 w-full px-2 lg:py-3 py-2 rounded-l-md text-gray-100 placeholder-gray-200 outline-none"
-              onChange={(e) => setName(e.target.value)}
-              placeholder="Enter your name"
-            />
-            <br />
-            <input
-              required
-              type="text"
-              className="bg-black/60 w-full px-2 lg:py-3 py-2 rounded-l-md text-gray-100 placeholder-gray-200 outline-none"
-              onChange={(e) => setRoom(e.target.value)}
-              placeholder="Enter room id"
-            />
-            <br />
-            <button
-              type="submit"
-              className="px-2 py-3 bg-red-500 rounded-lg text-sm w-full hover:bg-red-600 transition text-black font-semibold"
-              onClick={joinRoom}>
-              Join Room
-            </button>
+            <div className="space-y-6">
+              <h1 className="text-xl font-semibold text-gray-200">
+                Please fill these field to start the chat 💌
+              </h1>
+              <input
+                required
+                type="text"
+                className="bg-black/60 w-full px-2 lg:py-3 py-2 rounded-l-md text-gray-100 placeholder-gray-200 outline-none"
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Enter your name"
+              />
+              <br />
+              <input
+                required
+                type="text"
+                className="bg-black/60 w-full px-2 lg:py-3 py-2 rounded-l-md text-gray-100 placeholder-gray-200 outline-none"
+                onChange={(e) => setRoom(e.target.value)}
+                placeholder="Enter room id"
+              />
+              <br />
+              <button
+                type="submit"
+                className="px-2 py-3 bg-gradient-to-r from-black/40 via-rose-800 to-black/40 rounded-lg text-sm w-full  transition text-white font-semibold"
+                onClick={joinRoom}>
+                Join Room
+              </button>
+            </div>
           </>
         )}
         <br />
@@ -123,14 +127,14 @@ const App = () => {
               <h1 className=" text-gray-200 font-bold  w-full animate-pulse">
                 Live ChatBox
               </h1>
-              <p className="w-full text-end text-gray-200 font-semibold">
+              <p className="w-full text-end text-gray-200 text-sm mr-4 font-semibold">
                 <span className="text-green-200 ">&#x2022;</span> Connected
                 Users: {connectedUsers}
               </p>
             </div>
             <ul
               ref={chatBoxRef}
-              className="space-y-4 h-[400px]  overflow-y-scroll scrollbar  px-3">
+              className="space-y-4 h-[350px]  overflow-y-scroll scrollbar  px-3">
               {messageList.map((info, index) => (
                 <li key={index}>
                   {" "}
